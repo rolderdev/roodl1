c508e15546f5e2c9befe503bf63a5d0e8e73d458
Eric Tuvesson <eric.tuvesson@gmail.com>
2024-07-15 15:21:01 +0200

    chore: clean up user nodes (#53)

--- packages/noodl-viewer-react/src/nodes/std-library/user/login.js
+++ packages/noodl-viewer-react/src/nodes/std-library/user/login.js
@@ -1,18 +1,13 @@
 'use strict';
 
-const { Node, EdgeTriggeredInput } = require('@noodl/runtime');
 const UserService = require('./userservice');
 
-var LoginNodeDefinition = {
+const LoginNodeDefinition = {
   name: 'net.noodl.user.LogIn',
   docs: 'https://docs.noodl.net/nodes/data/user/log-in',
   displayNodeName: 'Log In',
   category: 'Cloud Services',
   color: 'data',
-  initialize: function () {
-    var internal = this._internal;
-  },
-  getInspectInfo() {},
   outputs: {
     success: {
       type: 'signal',
@@ -28,7 +23,7 @@ var LoginNodeDefinition = {
       type: 'string',
       displayName: 'Error',
       group: 'Error',
-      getter: function () {
+      getter() {
         return this._internal.error;
       }
     }
@@ -37,7 +32,7 @@ var LoginNodeDefinition = {
     login: {
       displayName: 'Do',
       group: 'Actions',
-      valueChangedToTrue: function () {
+      valueChangedToTrue() {
         this.scheduleLogIn();
       }
     },
@@ -45,7 +40,7 @@ var LoginNodeDefinition = {
       displayName: 'Username',
       type: 'string',
       group: 'General',
-      set: function (value) {
+      set(value) {
         this._internal.username = value;
       }
     },
@@ -53,13 +48,13 @@ var LoginNodeDefinition = {
       displayName: 'Password',
       type: 'string',
       group: 'General',
-      set: function (value) {
+      set(value) {
         this._internal.password = value;
       }
     }
   },
   methods: {
-    setError: function (err) {
+    setError(err) {
       this._internal.error = err;
       this.flagOutputDirty('error');
       this.sendSignalOnOutput('failure');
@@ -76,9 +71,7 @@ var LoginNodeDefinition = {
         this.context.editorConnection.clearWarning(this.nodeScope.componentOwner.name, this.id, 'user-login-warning');
       }
     },
-    scheduleLogIn: function () {
-      const internal = this._internal;
-
+    scheduleLogIn() {
       if (this.logInScheduled === true) return;
       this.logInScheduled = true;
 
@@ -100,89 +93,7 @@ var LoginNodeDefinition = {
   }
 };
 
-/*function updatePorts(nodeId, parameters, editorConnection, dbCollections) {
-    var ports = [];
-
-    ports.push({
-        name: 'collectionName',
-        displayName: "Class",
-        group: "General",
-        type: { name: 'enum', enums: (dbCollections !== undefined) ? dbCollections.map((c) => { return { value: c.name, label: c.name } }) : [], allowEditOnly: true },
-        plug: 'input'
-    })
-
-    if (parameters.collectionName && dbCollections) {
-        // Fetch ports from collection keys
-        var c = dbCollections.find((c) => c.name === parameters.collectionName);
-        if (c && c.schema && c.schema.properties) {
-            var props = c.schema.properties;
-            for (var key in props) {
-                var p = props[key];
-                if (ports.find((_p) => _p.name === key)) continue;
-
-                if(p.type === 'Relation') { 
-                    
-                }
-                else { // Other schema type ports
-                    const _typeMap = {
-                        "String":"string",
-                        "Boolean":"boolean",
-                        "Number":"number",
-                        "Date":"date"
-                    }
-
-                    ports.push({
-                        type: {
-                            name: _typeMap[p.type]?_typeMap[p.type]:'*',
-                        },
-                        plug: 'output',
-                        group: 'Properties',
-                        name: 'prop-' + key,
-                        displayName: key,
-                    })
-
-                    ports.push({
-                        type: 'signal',
-                        plug: 'output',
-                        group: 'Changed Events',
-                        displayName: key+ ' Changed',
-                        name: 'changed-' + key,
-                    })
-                }
-            }
-        }
-    }
-
-     editorConnection.sendDynamicPorts(nodeId, ports);
-}*/
-
 module.exports = {
   node: LoginNodeDefinition,
-  setup: function (context, graphModel) {
-    /* if (!context.editorConnection || !context.editorConnection.isRunningLocally()) {
-            return;
-        }
-
-        function _managePortsForNode(node) {
-            updatePorts(node.id, node.parameters, context.editorConnection, graphModel.getMetaData('dbCollections'));
-
-            node.on("parameterUpdated", function (event) {
-                updatePorts(node.id, node.parameters, context.editorConnection, graphModel.getMetaData('dbCollections'));
-            });
-
-            graphModel.on('metadataChanged.dbCollections', function (data) {
-                updatePorts(node.id, node.parameters, context.editorConnection, data);
-            })
-        }
-
-        graphModel.on("editorImportComplete", ()=> {
-            graphModel.on("nodeAdded.DbModel2", function (node) {
-				_managePortsForNode(node)
-			})
-
-			for(const node of graphModel.getNodesWithType('DbModel2')) {
-				_managePortsForNode(node)
-			}
-        })*/
-  }
+  setup(_context, _graphModel) {}
 };
